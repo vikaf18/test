a0ee999511e22eba1bcb8c5451dba6a05d8db77b
vika <viktoriafedorova18072003@gmail.com>
2023-01-13 14:30:06 +0300

    init react app

---
+++ src/Header.jsx
@@ -0,0 +1,25 @@
+// function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//         </header>
+//     );
+// }
+
+// export default Header;
+
+// function Header() {
+//     return (
+//         <header>
+//             <h1>Header</h1>
+//         </header>
+//     );
+// }
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Header</h1>
+        </header>
+    );
+}
