e3a231058ad6e7607b42a5dc3e576f58d5eddc22
vika <viktoriafedorova18072003@gmail.com>
2023-01-13 14:40:28 +0300

    Add folder components, update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { Header } from './header'
+import { Header } from './components/Header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
